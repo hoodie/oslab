322df677dbaeff709e80edb669d092ff3b3be4b4
Hendrik Sollich <Hendrik@hoodie.de>
2012-01-16 19:15:25 +0100

    running hello server

---
+++ src/l4/pkg/hello_server/client.cc
@@ -0,0 +1,57 @@
+/*
+ * (c) 2008-2009 Adam Lackorzynski <adam@os.inf.tu-dresden.de>,
+ *               Alexander Warg <warg@os.inf.tu-dresden.de>
+ *     economic rights: Technische Universität Dresden (Germany)
+ *
+ * This file is part of TUD:OS and distributed under the terms of the
+ * GNU General Public License 2.
+ * Please see the COPYING-GPL-2 file for details.
+ *
+ * Adapted by Hendrik Sollich
+ */
+#include <l4/sys/err.h>
+#include <l4/sys/types.h>
+#include <l4/re/env>
+#include <l4/re/util/cap_alloc>
+#include <l4/cxx/ipc_stream>
+
+#include <stdio.h>
+
+#include "shared.h"
+
+
+
+int func_hello_call(L4::Cap<void> const &server) {
+
+  L4::Ipc::Iostream stream(l4_utcb());
+  stream << l4_umword_t(Opcode::func_hello);
+  stream << "ping_pong!" ; // 10 Characters
+  l4_msgtag_t res = stream.call(server.cap(), Protocol::Hello);
+  if (l4_ipc_error(res, l4_utcb()))
+    return 1; // failure
+  return 0; // ok
+}
+
+
+int main() {
+
+  // query IPC-Gate at name service > capability
+  L4::Cap<void> server = L4Re::Env::env()->get_cap<void>("channel");
+
+  if (!server.is_valid()) {
+    printf("Could not get server capability!\n");
+    return 1;
+  }
+
+  if (func_hello_call(server)) {
+    printf("Server does not respond correctly\n");
+    return 1;
+  }
+  else {
+    printf("Sent successfully!\n");
+  
+  }
+
+
+  return 0;
+}
---
+++ src/l4/pkg/hello_server/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp modules.list ../../conf/modules.list
